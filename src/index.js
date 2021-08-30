cd5d8091794ee5484332a60c47b941ee1533cfc0
zFarrukh <66554030+zFarrukh@users.noreply.github.com>
2021-08-30 14:58:30 +0500

    fixed typo user selector and created new reducer to directory component's state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import store from "./redux/store"
+import { persistor } from './redux/store';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router} from 'react-router-dom'
 import { Provider } from 'react-redux';
-
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </Router>
     </Provider>
   </React.StrictMode>,

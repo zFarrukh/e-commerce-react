341351c811c2975b0c4fe7e47ecadab6e20bb86d
zFarrukh <66554030+zFarrukh@users.noreply.github.com>
2021-09-10 14:23:57 +0500

    updating our code to be pwa compliant, added serviceworker, https, updated manifest

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import store from "./redux/store"
-import { persistor } from './redux/store';
+import store, { persistor } from "./redux/store"
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router} from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
+import * as serviceWorker from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -20,3 +20,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
 
 if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
@@ -15,6 +16,7 @@ const port = process.env.PORT || 5000;
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({trustProtoHeader: true}));
 
 app.use(cors());
 
@@ -30,6 +32,10 @@ app.listen(port, err => {
     if(err) throw err;
     console.log('Server running on port ' + port);
 });
+
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+})
 
 app.post('/payment', (req, res) => {
     const body = {
